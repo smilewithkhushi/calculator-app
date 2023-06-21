b018d3fde541a8670db204ef93a8b9ed4733cd05
Khushi <exotica.xo1@gmail.com>
2023-06-21 17:26:57 +0530

    final project files

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : AppCompatActivity() {
         }
         buttonClear.setOnClickListener {
             finalresult.text = " "
+            resultTextView.text=" "
         }
 
         buttonEquals.setOnClickListener {
